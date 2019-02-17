f27f0360d3a02c92e996ce2da2ae72bb44a34f8d
furuya@MBP <takashi.furuya.0@gmail.com>
2019-02-17 17:16:58 +0900

    modified

--- ra/views.py
+++ ra/views.py
@@ -1,12 +1,15 @@
 from django.template.response import TemplateResponse
 from google.cloud import datastore
+from ra.functions import f_datastore
 
 # Create your views here.
 def main(request):
-    client = datastore.Client()
-    query = client.query(kind='Kakeibo')
-    query.order = ["-date"]
-    data = list(query.fetch(10))
+    # client = datastore.Client()
+    # query = client.query(kind='Kakeibo')
+    # query.order = ["-date"]
+    # data = list(query.fetch(10))
+    fds = f_datastore.fds("Kakeibo")
+    data = fds.all().order("-date").get()[0:10]
     output = {
         "msg": "Hello world",
         "data": data,
