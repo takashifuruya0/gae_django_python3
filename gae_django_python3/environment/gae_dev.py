783a802a8cdcb1fb8a9f9ea0972c6ce406c46bce
furuya <>
2019-03-07 17:48:18 +0900

    gaebase_devを作成し、Photoモデルを実装・マイグレーション

--- gae_django_python3/environment/gae_dev.py
+++ gae_django_python3/environment/gae_dev.py
@@ -3,7 +3,7 @@ from gae_django_python3.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "gaebase",
+        'NAME': "gaebase_dev",
         "USER": 'gaeuser',
         "PASSWORD": 'Takashi&102',
         'HOST': "35.207.8.90",
--- ra/models.py
+++ ra/models.py
@@ -14,18 +14,24 @@ class habit(models.Model):
 
 
 class Photo(models.Model):
+    objects = None
+    datetime = models.DateTimeField()
+    sitename = models.CharField(max_length=30, blank=False)
+    prefecture = models.CharField(max_length=30, blank=False)
+    country = models.CharField(max_length=30, blank=False)
     comment = models.CharField(max_length=100, blank=True)
-    sitename = models.CharField(max_length=100, blank=True)
-    prefecture = models.CharField(max_length=100, blank=True)
-    country = models.CharField(max_length=100)
+    # path
     path = models.CharField(max_length=100)
     path_resized = models.CharField(max_length=100)
     path_resized_480 = models.CharField(max_length=100)
     path_resized_1200 = models.CharField(max_length=100)
-    datetime = models.DateTimeField()
-    landmark = models.CharField(max_length=100, blank=True)
+    # VisionAPI
+    landmark = models.CharField(max_length=30, blank=True)
     latitude = models.FloatField(blank=True)
     longitude = models.FloatField(blank=True)
     score = models.FloatField(blank=True)
     is_api_called = models.BooleanField(default=False)
-    tags = ArrayField(models.CharField(max_length=100), blank=True)
+    tags = ArrayField(models.CharField(max_length=30), blank=True, default=list)
+
+    def __str__(self):
+        return self.name
