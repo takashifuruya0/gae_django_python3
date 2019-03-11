4ece95d51cab4c874c0b5d003bc963c0b446918e
furuya@MBP <takashi.furuya.0@gmail.com>
2019-03-11 21:33:22 +0900

    app.yaml修正

--- gae_django_python3/environment/gae.py
+++ gae_django_python3/environment/gae.py
@@ -3,11 +3,11 @@ from gae_django_python3.settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': "gaebase",
-        "USER": 'gaeuser',
-        "PASSWORD": 'Takashi&102',
-        'HOST': "35.207.8.90",
-        "PORT": "5432",
+        'NAME': os.environ['_DB_NAME'],
+        "USER": os.environ['_DB_USER'],
+        "PASSWORD": os.environ['_DB_PASSWORD'],
+        'HOST': os.environ['_DB_HOST'],
+        "PORT": os.environ['_DB_PORT'],
         "ATOMIC_REQUESTS": True,
     }
 }
@@ -15,4 +15,4 @@ DATABASES = {
 ENVIRONMENT = "gae"
 DEBUG = False
 
-GOOGLE_MAPS_API_KEY = os.environ
+GOOGLE_MAPS_API_KEY = "AIzaSyD7BCPVSpQ6Xw2li13WK0O0TCWrB_E3dLE"
