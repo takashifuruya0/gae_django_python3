1c29d9c18fab73bd0248fd2b18f3ffa4ea38e0d9
furuya <>
2019-03-12 12:44:43 +0900

    postgre接続をあきらめてみた

--- gae_django_python3/environment/gae.py
+++ gae_django_python3/environment/gae.py
@@ -1,14 +1,20 @@
 from gae_django_python3.settings import *
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': os.environ['_DB_NAME'],
+#         "USER": os.environ['_DB_USER'],
+#         "PASSWORD": os.environ['_DB_PASSWORD'],
+#         'HOST': os.environ['_DB_HOST'],
+#         "PORT": os.environ['_DB_PORT'],
+#         "ATOMIC_REQUESTS": True,
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['_DB_NAME'],
-        "USER": os.environ['_DB_USER'],
-        "PASSWORD": os.environ['_DB_PASSWORD'],
-        'HOST': os.environ['_DB_HOST'],
-        "PORT": os.environ['_DB_PORT'],
-        "ATOMIC_REQUESTS": True,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
